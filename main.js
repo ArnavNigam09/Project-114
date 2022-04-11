e613642a6c64a5ef5df24e4495fa82752ee84264
ArnavNigam09 <83456570+ArnavNigam09@users.noreply.github.com>
2022-04-11 19:42:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function take_snapshot()
+{
+    save('myfilterimage.png')
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function preload()
+{
+
+}
+
+function draw()
+{
+   
+}
